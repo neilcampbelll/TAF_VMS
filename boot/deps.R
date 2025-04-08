bf2c62ba6375785c7befc764b86dd3ee176fef21
Neil Campbell <neil.campbell@ices.dk>
2025-04-08 16:05:11 +0200

    updated repose

--- boot/deps.R
+++ boot/deps.R
@@ -1,16 +1,22 @@
 
 library(icesTAF)
 
+# Add this before installing packages
+options(repos = c(
+  ICES = "https://ices-tools-prod.r-universe.dev",
+  CRAN = "https://cloud.r-project.org"
+))
+
 # install deps from TAF boot scripts
 install.deps(
   path = taf.boot.path(),
-  repos = c("https://ices-tools-prod.r-universe.dev", "https://cloud.r-project.org")
+  repos = c("https://cloud.r-project.org")
 )
 
 # install deps from TAF scripts
 install.deps(
   path = taf.boot.path(".."),
-  repos = c("https://ices-tools-prod.r-universe.dev", "https://cloud.r-project.org")
+  repos = c("https://cloud.r-project.org")
 )
 
 # additional check
@@ -23,6 +29,5 @@ pacman::p_load(
   sf, data.table, raster, terra, mapview, Matrix, dplyr,
   doBy, mixtools, tidyr, glue, gt, progressr, geosphere, purrr,
   ggplot2, sfdSAR, icesVocab, generics, icesConnect, icesVMS, icesSharePoint,
-  tidyverse, units, tcltk, lubridate, here,
-  repos = c("https://ices-tools-prod.r-universe.dev", "https://cloud.r-project.org")
-)
+  tidyverse, units, tcltk, lubridate, here)
+
